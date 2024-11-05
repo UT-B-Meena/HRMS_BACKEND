2d53f73df3cb8248f2e4eae8f269133a544f47ba
UT-B-Kathija <kathijabeevi@theunitytech.com>
2024-11-05 09:14:42 +0530

    employee attendance

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,11 +10,15 @@ class Product extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['name','status', 'created_by', 'updated_by'];
+    protected $fillable = ['name','status','created_by', 'updated_by'];
 
     public function projects()
     {
         return $this->hasMany(Project::class);
+    }
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
     }
 
     public function updatedBy()
@@ -26,4 +30,15 @@ class Product extends Model
     {
         return $this->belongsTo(User::class, 'created_by');
     }
+    public function subTasks()
+    {
+        return $this->hasMany(SubTask::class, 'product_id');
+    }
+
+    const STATUS = [
+        0 => 'Get to Start',
+        1 => 'In Progress',
+        2 => 'Hold',
+        3 => 'Completed'
+    ];
 }
