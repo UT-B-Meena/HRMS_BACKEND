b7194cccedcc0b9bbe38716b7839c0992878d885
kathija-aafiya29 <kathijabee29@gmail.com>
2024-11-19 10:28:51 +0530

    comments column added

--- database/migrations/2024_11_19_094620_create_task_comments_table.php
+++ database/migrations/2024_11_19_094620_create_task_comments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('task_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('comments');
             $table->foreignId('updated_by')->constrained('users')->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes(); // Adds a deleted_at column for soft deleting
